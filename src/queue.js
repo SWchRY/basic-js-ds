7d2660e82c9b5ef0400d5637a091f3d95a60755f
SwchRY <urkevich.alex.chr@mail.ru>
2022-10-18 01:38:49 +0300

    feat: solved

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (!this.value) {
+      console.log(1111111)
+      this.value = value;
+      return this.value;
+    }
+    let last = this;
+    while (last.next) {
+      last = last.next
+    }
+    last.next = new ListNode (value);
+    return this.value;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let value = this.value;
+    this.value = this.next.value;
+    this.next = this.next.next;
+    return value;
   }
 }
 
